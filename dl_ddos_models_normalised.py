25eab2aebe9f89ff281b3a607f92208c4ada1e50
JishaMansoor <74047648+JishaMansoor@users.noreply.github.com>
2023-03-25 14:47:59 +0530

    Update dl_ddos_models_normalised.py
    Added Author information

--- dl_ddos_models_normalised.py
+++ dl_ddos_models_normalised.py
@@ -1,3 +1,26 @@
+
+# Author: Jisha C U
+# Project: CYBER SECURITY: NEAR REAL TIME DDoS CYBER ATTACK DETECTION USING DEEP LEARNING METHODS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#Model Training
+#python3 dl_ddos_models_normalised.py --train ~/final_data_syn --modelname SBL_A
+#python3 dl_ddos_models_normalised.py  --train ~/final_data_syn --modelname BI_GRU_ATTN
+#python3 dl_ddos_models_normalised.py  --train ~/final_data_syn --modelname SBLBG_A
+
+# Model Testing
+#python3 dl_ddos_models_normalised.py --predict ./final_dataset/ --model ./output/10t-10n-IDS201X-BI_GRU_ATTN.h5
+
 import tensorflow as tf
 import numpy as np
 import random as rn
